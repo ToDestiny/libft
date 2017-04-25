8fe544f1301b69719d8eb7f6f2aa9a43868c71a5
Anthony COLAS <anthonycolas.france@gmail.com>
2017-04-25 20:31:26 +0200

    atoi v2

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,37 +6,53 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/25 18:54:41 by acolas            #+#    #+#             */
-/*   Updated: 2017/04/25 20:04:20 by acolas           ###   ########.fr       */
+/*   Updated: 2017/04/25 20:30:18 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int		ft_number(const char *str, unsigned int i, int neg)
+{
+	unsigned long	num;
+	
+	num = 0;	
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	if (neg == 1)
+		return (num * -1);
+	return (0);
+}
+
 int		ft_atoi(const char *str)
 {
-	int				i;
-	int				len;
-	unsigned int	num;
+	int	i;
+	int	len;
+	int	neg;
 
 	if (!(*str))
 	{
 		len = ft_strlen(str);
 		i = 0;
+		neg = 0;
 		while (len > 0)
 		{
-			while (str[i++] <= 32)
-			if (str[i++] == '-')
-				ft_putchar('-');
-			else if (str[i++] == '+')
-			while (str[i] >= '0' && str[i] <= '9')
+			while (str[i] <= 32)
+				i++;
+			if (str[i] == '-')
 			{
-				num = num * 10 + (str[i] - '0');
+				neg = 1;
+				ft_putchar('-');
 				i++;
 			}
-			return (num);
+			else if (str[i] == '+')
+				i++;
+			return (ft_number(str, i ,neg));
 		}
 		return (0);
 	}
-	else
-		return (0);
+	return (0);
 }
