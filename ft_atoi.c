e4b7a01b674bf67ca63fb36b77224db062140880
Anthony COLAS <acolas@e1r8p9.42.fr>
2017-04-26 15:40:36 +0200

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,3 +1,57 @@
+<<<<<<< HEAD
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/25 18:54:41 by acolas            #+#    #+#             */
+/*   Updated: 2017/04/26 15:29:47 by acolas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_number(const char *str, unsigned long long int i, int neg)
+{
+	unsigned long long int	num;
+
+	num = 0;	
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	if (neg == 1)
+	{
+		num = -num;
+		return (num);
+	}
+	return (num);
+}
+
+int		ft_atoi(const char *str)
+{
+	unsigned long long int	i;
+	int						neg;
+
+	i = 0;
+	neg = 0;
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-')
+	{
+		neg = 1;
+		ft_putchar('-');
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	return (ft_number(str, i ,neg));
+}
+=======
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -56,3 +110,4 @@ int		ft_atoi(const char *str)
 	}
 	return (0);
 }
+>>>>>>> 1a3aa467c0d90cd80624ec2aecdd3e1ddb48d755
