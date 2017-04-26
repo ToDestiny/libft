f0589d49651cf44e0d62876522341f9d1acf5689
Anthony COLAS <acolas@e1r8p9.42.fr>
2017-04-26 15:51:21 +0200

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/26 15:43:26 by acolas            #+#    #+#             */
+/*   Updated: 2017/04/26 15:50:36 by acolas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char		*ft_strdup(const char *str)
+{
+	int		i;
+	char	*dest;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(str);
+	dest = (char *)malloc(sizeof(*dest) * len + 1);
+	if (dest == 0)
+		return (0);
+	while (len > 0)
+	{
+		dest[i] = str[i];
+		i++;
+		len--;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
