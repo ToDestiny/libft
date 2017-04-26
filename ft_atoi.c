b469fef161cd428c115300ceff491db7af32b96d
Anthony COLAS <acolas@e1r8p9.42.fr>
2017-04-26 15:42:39 +0200

    atoiv3

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -7,7 +6,7 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/25 18:54:41 by acolas            #+#    #+#             */
-/*   Updated: 2017/04/26 15:29:47 by acolas           ###   ########.fr       */
+/*   Updated: 2017/04/26 15:41:49 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,63 +50,3 @@ int		ft_atoi(const char *str)
 		i++;
 	return (ft_number(str, i ,neg));
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/25 18:54:41 by acolas            #+#    #+#             */
-/*   Updated: 2017/04/25 20:30:18 by acolas           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-int		ft_number(const char *str, unsigned int i, int neg)
-{
-	unsigned long	num;
-	
-	num = 0;	
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		num = num * 10 + (str[i] - '0');
-		i++;
-	}
-	if (neg == 1)
-		return (num * -1);
-	return (0);
-}
-
-int		ft_atoi(const char *str)
-{
-	int	i;
-	int	len;
-	int	neg;
-
-	if (!(*str))
-	{
-		len = ft_strlen(str);
-		i = 0;
-		neg = 0;
-		while (len > 0)
-		{
-			while (str[i] <= 32)
-				i++;
-			if (str[i] == '-')
-			{
-				neg = 1;
-				ft_putchar('-');
-				i++;
-			}
-			else if (str[i] == '+')
-				i++;
-			return (ft_number(str, i ,neg));
-		}
-		return (0);
-	}
-	return (0);
-}
->>>>>>> 1a3aa467c0d90cd80624ec2aecdd3e1ddb48d755
