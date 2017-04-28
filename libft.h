37c990dc800c9ab46552fe12017cc1debae83acc
Anthony COLAS <acolas@e2r3p7.42.fr>
2017-04-28 19:51:37 +0200

    lib

--- libft.h
+++ libft.h
@@ -46,5 +46,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *str, int c, int n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 
 #endif
