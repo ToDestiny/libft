5c389f30f671626f90e4547f9ba1b1e205927d4a
Anthony COLAS <acolas@e1r4p12.42.fr>
2017-04-29 18:02:47 +0200

    memdel

--- libft.h
+++ libft.h
@@ -48,5 +48,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
 
 #endif
