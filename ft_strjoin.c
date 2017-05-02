ce41831c3860120d3d8d7e999a9c77ff9c866f55
Anthony COLAS <acolas@e2r8p6.42.fr>
2017-05-02 18:44:39 +0200

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	len;
+	char			*dst;
+
+	if (s1 && s2)
+	{
+		len = ft_strlen(s1) + ft_strlen(s2);
+		dst = (char *)malloc(sizeof(*dst) * (len + 1));
+		if (dst == 0)
+			return (NULL);
+		ft_strcpy(dst, s1);
+		ft_strcat(dst, s2);
+		return (dst);
+	}
+	return (0);
+}
