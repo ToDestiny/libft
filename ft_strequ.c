9f934ec74fcc3a998feb35a3e7692010249176db
Anthony COLAS <acolas@e2r8p6.42.fr>
2017-05-02 17:41:18 +0200

    strequ + strmapi

--- ft_strequ.c
+++ ft_strequ.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int		ft_strequ(char const *s1, char const *s2)
+{
+	int		i;
+	int		len1;
+	int		len2;
+
+	if (s1 && s2)
+	{
+		i = 0;
+		len1 = ft_strlen(s1);
+		len2 = ft_strlen(s2);
+		while (len1 > 0 || len2 > 0)
+		{
+			if (s1[i] == s2[i])
+			{
+				i++;
+				len1--;
+				len2--;
+			}
+			else
+				return (0);
+		}
+		return (1);
+	}
+	return (0);
+}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int		i;
+	unsigned int		len;
+	char				*dst;
+
+	if (s)
+	{
+		i = 0;
+		len = ft_strlen(s);
+		dst = (char *)malloc(len + 1);
+		if (dst == 0)
+			return (0);
+		while (i < len)
+		{
+			dst[i] = (*f)(i, s[i]);
+			i++;
+		}
+		dst[i] = '\0';
+		return (dst);
+	}
+	return (0);
+}
