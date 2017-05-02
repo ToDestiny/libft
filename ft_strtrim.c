e9ced79905bf40c87c6c8dfb15f1a9b7431d1a7b
Anthony COLAS <acolas@e2r8p6.42.fr>
2017-05-02 20:45:32 +0200

    strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,4 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strtrim(char const *s)
+{
+	char const			*end;
+	char const			*beg;
+	char				*dst;
+	unsigned int		i;
+
+	if (s)
+	{
+		i = 0;
+		while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+			i++;
+		beg = (s + i);
+		end = (s + i);
+		while (s[i])
+		{
+			if (s[i] >= 33 && s[i] <= 126)
+				end = s + i;
+			i++;
+		}
+		if ((end - beg) > 0)
+			end++;
+		dst = ft_strsub(s, beg - s, end - beg);
+		return (dst);
+	}
+	return (0);
+}
