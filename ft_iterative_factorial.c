e80bae97ff571c40500c75652dbecda245bdd0b1
Anthony COLAS <acolas@e2r9p11.42.fr>
2017-05-05 17:50:19 +0200

    bonus 1

--- ft_iterative_factorial.c
+++ ft_iterative_factorial.c
@@ -1,18 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acolas <acolas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/05 17:37:14 by acolas            #+#    #+#             */
-/*   Updated: 2017/05/05 17:37:18 by acolas           ###   ########.fr       */
+/*   Created: 2017/05/05 17:43:44 by acolas            #+#    #+#             */
+/*   Updated: 2017/05/05 17:43:46 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+int		ft_iterative_factorial(int n)
 {
-	
+	int		i;
+	int		res;
+
+	i = 1;
+	res = 1;
+	if (n < 0 || n > 12)
+	{
+		i = 0;
+		return (i);
+	}
+	else if (n == 0)
+	{
+		i = 1;
+		return (i);
+	}
+	while (i <= n)
+	{
+		res = res * i;
+		i++;
+	}
+	return (res);
 }
