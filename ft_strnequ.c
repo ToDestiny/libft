296df68998ce78f9efa8a6116e82dbace57383c4
Anthony COLAS <acolas@e2r9p11.42.fr>
2017-05-05 16:20:17 +0200

    strnequ v2

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -14,28 +14,5 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	int		i;
-	int		len1;
-	int		len2;
-
-	if (s1 && s2)
-	{
-		i = 0;
-		len1 = ft_strlen(s1);
-		len2 = ft_strlen(s2);
-		while (len1 > 0 || len2 > 0 || n > 0)
-		{
-			if (s1[i] == s2[i])
-			{
-				i++;
-				len1--;
-				len2--;
-				n--;
-			}
-			else
-				return (0);
-		}
-		return (1);
-	}
-	return (0);
+	return (!ft_strncmp(s1, s2, n));
 }
